749f300d391086911ee455bcf8eb47a16f3be824
toktonaliev1 <toktonalievamantur66@gmail.com>
2022-08-09 16:15:45 +0300

    добавил androidEntrypoin in mainactivity реализовал  setupNavigation

---
+++ app/src/main/java/com/example/opendota/ui/activity/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.opendota.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.example.opendota.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+    }
+}
